c5e40afc1d595d59cffc29859bd1ce1be23080e4
uditha <udithaindunil5@gmail.com>
2023-09-26 11:04:18 +0530

    post and get rq

--- Controllers/movieController.js
+++ Controllers/movieController.js
@@ -1,27 +1,58 @@
-const fs = require("fs");
-const Movie=require('../Model/movieModel')
+const Movie = require("../Model/movieModel");
 
+//get the all movies
+exports.getallMovie = async (req, res) => {
+  try {
+    const movie = await Movie.find();
+    res.status(200).json({
+      message: "find",
+      data: {
+        movie,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
 
+//filter with some data using the req id
+exports.singleMovie = async (req, res) => {
+  try {
+    const movie = await Movie.findById(req.params.id);
+    res.status(200).json({
+      message: "find",
+      data: {
+        movie,
+      },
+    });
+  } catch (err) {
+   res.status(404).json({
+    status:"fail",
+    message:err.message
+   })
+  }
+};
+exports.patchReq = (req, res) => {};
 
+exports.deleteMovie = (req, res) => {};
 
-
-exports.getallMovie = (req, res) => {
-   
-  };
-  
-exports.singleMovie = (req, res) => {
-   
-  };
-  exports.patchReq = (req, res) => {
-  
-  };
-  
-exports.deleteMovie = (req, res) => {
-   
-  };
-  
-exports.postMovie = (req, res) => {
-  
-    
-  };
-
+//insert the movie to the mongo db
+exports.postMovie = async (req, res) => {
+  try {
+    const movie = await Movie.create(req.body);
+    res.status(201).json({
+      status: "create",
+      data: {
+        movie,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
